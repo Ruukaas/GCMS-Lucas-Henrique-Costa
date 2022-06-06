2f444d49c98dc3e13607213af1b04e3505f6e49d
Ruukaas <lucashenriquehca@gmail.com>
2022-06-05 23:48:02 -0300

    Segundo Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá mundo");
-        System.out.println("Segunda modificação");
+        System.out.println("Ola mundo");
+        System.out.println("Segunda modificacao");
+        System.out.println("Terceira modificacao");
     }
 }
